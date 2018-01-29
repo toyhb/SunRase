eefa049ccce2c28adee83ab77f5ee3c755d6f1af
lolxnn <inkyincolto@gmail.com>
2018-01-29 15:24:48 +0100

    Sketch completo e compilabile
    Non ancora testato su SunRase.

--- Sunrase_4.0_Motori.ino
+++ Sunrase_4.0_Motori.ino
@@ -1,14 +1,22 @@
 /*
  * SunRase 4.0 by: Reolfi Riccardo, Incolto Davide, Murialdo Andrea, Pastorino Simone.
- * Code by: Davide Incolto.
+ * Code by: Incolto Davide.
 */
 int motDestraA = 2;
 int motDestraI = 3;
 int motSinistraA = 4;
 int motSinistraI = 5;
 int magnetic = 6;
-int triggerUp = 9;
-int echoUp = 10;
+int triggerUp = 7;
+int echoUp = 8;
+int triggerDestra = 9;
+int echoDestra = 10;
+int triggerSinistra = 11;
+int echoSinistra = 12;
+int tempo_rotazione = 1500;
+long durata;
+long distanza;
+int val;
 void setup() {
   pinMode(motDestraI, OUTPUT);
   pinMode(motDestraA, OUTPUT);
@@ -17,7 +25,12 @@ void setup() {
   pinMode(magnetic, INPUT);
   pinMode(triggerUp, OUTPUT);
   pinMode(echoUp, INPUT);
+  pinMode(triggerDestra, OUTPUT);
+  pinMode(echoDestra, INPUT);
+  pinMode(triggerSinistra, OUTPUT);
+  pinMode(echoSinistra, INPUT);
   Serial.begin(9600);
+  randomSeed(analogRead(0));
 }
 void loop() {
   AvantiTutta();
@@ -38,11 +51,11 @@ bool checkMag(){
  } 
 }
 bool checkUltra(){
-  digitalWrite( triggerPort, LOW );
-  digitalWrite( triggerPort, HIGH );
+  digitalWrite( triggerUp, LOW );
+  digitalWrite( triggerUp, HIGH );
   delayMicroseconds( 10 );
-  digitalWrite( triggerPort, LOW );
-  long durata = pulseIn( echoPort, HIGH );
+  digitalWrite( triggerUp, LOW );
+  long durata = pulseIn( echoUp, HIGH );
   long distanza = 0.034 * durata / 2;
   Serial.println(distanza);
   if(distanza < 10){
@@ -53,6 +66,96 @@ bool checkUltra(){
  }
 }
 void AvantiTutta(){
+  digitalWrite(motDestraI, LOW);
+  digitalWrite(motSinistraI, LOW);
+  delay(10);
   digitalWrite(motDestraA, HIGH);
   digitalWrite(motSinistraA, HIGH);
 }
+void gira(){
+  val = lateralCheck();
+  if(val = 1){
+  digitalWrite(motDestraA, LOW);
+  digitalWrite(motSinistraA, LOW);
+  delay(500);
+  digitalWrite(motDestraI, HIGH);
+  digitalWrite(motSinistraI, HIGH);
+  delay(1000);
+  digitalWrite(motDestraA, LOW);
+  digitalWrite(motSinistraA, LOW);
+  delay(10);
+  digitalWrite(motDestraA, HIGH);
+  digitalWrite(motSinistraA, LOW);
+  delay(tempo_rotazione); 
+  digitalWrite(motDestraA, LOW);
+  digitalWrite(motSinistraA, LOW);
+  delay(10);    
+ }
+ else if(val = 2){
+  digitalWrite(motDestraA, LOW);
+  digitalWrite(motSinistraA, LOW);
+  delay(500);
+  digitalWrite(motDestraI, HIGH);
+  digitalWrite(motSinistraI, HIGH);
+  delay(1000);
+  digitalWrite(motDestraA, LOW);
+  digitalWrite(motSinistraA, LOW);
+  delay(10);
+  digitalWrite(motDestraA, HIGH);
+  digitalWrite(motSinistraA, LOW);
+  delay(tempo_rotazione); 
+  digitalWrite(motDestraA, LOW);
+  digitalWrite(motSinistraA, LOW);
+  delay(10);
+  }
+}
+int lateralCheck(){
+  int x;
+  if(destraCheck == true){
+   x = 1;
+   return x;
+ }
+ else if(sinistraCheck == true){
+   x = 2;
+   return x;
+ }
+ delay(10);
+ int randNumber = random(300);
+ if(randNumber < 150){
+  x = 1;
+  return x;
+  }
+ else if(randNumber > 149){
+  x = 0;
+  return x;
+  }
+ 
+}
+bool destraCheck(){
+  digitalWrite(triggerDestra, LOW);
+  digitalWrite(triggerDestra, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerDestra, LOW);
+  durata = pulseIn(echoDestra, HIGH);
+  distanza = 0.034 * durata / 2;
+  if(distanza < 40){
+  return true;  
+  }
+  else if(distanza > 39){
+  return false;  
+  } 
+ }
+ bool sinistraCheck(){
+  digitalWrite(triggerSinistra, LOW);
+  digitalWrite(triggerSinistra, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerSinistra, LOW);
+  durata = pulseIn(echoSinistra, HIGH);
+  distanza = 0.034 * durata / 2;
+  if(distanza < 40){
+  return true;  
+  }
+  else if(distanza > 39){
+  return false;  
+  } 
+ }
