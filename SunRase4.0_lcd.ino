141c3b68f1ce3d204fcac57337b1ea0eb34be1d7
lolxnn <inkyincolto@gmail.com>
2018-02-02 22:29:45 +0100

    Rimossa funzione inutile e aggiunta una utile

--- SunRase4.0_lcd.ino
+++ SunRase4.0_lcd.ino
@@ -1,42 +1,31 @@
 #include <LiquidCrystal.h>
+const int tempPin = A0;
+const int batPin = A1;
 LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
 void setup() {
-  // put your setup code here, to run once:
   lcd.begin(16, 2);
   pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
- lcd.print("Luminosita:");
- lcd.print(analogRead(A0));
- delay(400);
+ lcd.setCursor(0, 0);
+ lcd.print("Temp. Driver:");
+ lcd.print(temp());
+ lcd.setCursor(0, 1);
+ lcd.print("Batteria:");
+ lcd.print(batteria());
+ lcd.print("%");
  lcd.clear();
-
-
 }
-long readVcc() {
-  // Read 1.1V reference against AVcc
-  // set the reference to Vcc and the measurement to the internal 1.1V reference
-  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-    ADMUX = _BV(MUX5) | _BV(MUX0);
-  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
-    ADMUX = _BV(MUX3) | _BV(MUX2);
-  #else
-    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  #endif  
- 
-  delay(2); // Wait for Vref to settle
-  ADCSRA |= _BV(ADSC); // Start conversion
-  while (bit_is_set(ADCSRA,ADSC)); // measuring
- 
-  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
-  uint8_t high = ADCH; // unlocks both
- 
-  long result = (high<<8) | low;
- 
-  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
-  return result; // Vcc in millivolts
+float temp(){
+  int tempVal = analogRead(tempPin);
+  float millivolts = (tempVal / 1024.0) * 5000;
+  float celsius = millivolts / 10;
+  return celsius;
+}
+int batteria(){
+  int batVal = analogRead(batPin);
+  int batX100 = (batVal * 100) / 1024;
+  return batX100;
 }
